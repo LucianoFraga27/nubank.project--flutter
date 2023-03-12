5b87639384362df59d34c079ba4c457441e772c3
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-03-12 19:30:22 -0300

    Detalhes do 'pegar emprestimos'

---
+++ lib/pages/homepage/icones.dart
@@ -0,0 +1,275 @@
+import 'package:flutter/material.dart';
+
+class IconesPage extends StatelessWidget {
+  const IconesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        //color: Colors.red,
+        height: 130,
+        width: double.infinity,
+        margin: EdgeInsets.only(left: 10),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 70,
+                          width: 70,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 238, 238, 238),
+                              borderRadius: BorderRadius.circular(100)),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                color: Colors.black,
+                                Icons.pix,
+                                size: 30,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 5),
+                          child: Text(
+                            "Pix",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 238, 238, 238),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            color: Colors.black,
+                            Icons.bookmarks_sharp,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Text(
+                        "Pagar",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 238, 238, 238),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Stack(
+                            children: [
+                              const Positioned(
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0,
+                                child: Icon(
+                                  color: Colors.black,
+                                  Icons.attach_money_rounded,
+                                  size: 30,
+                                ),
+                              ),
+                              Column(
+                                children: [
+                                  Container(
+                                    width: double.infinity,
+                                    height: 50,
+                                    color: Color.fromARGB(0, 197, 88, 41),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.purple,
+                                      borderRadius: BorderRadius.circular(3),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.1),
+                                          spreadRadius: 0.1,
+                                          blurRadius: 0.1,
+                                          offset: const Offset(0, 0.2),
+                                        ),
+                                      ],
+                                    ),
+                                    width: double.infinity,
+                                    height: 18,
+                                    child: Center(
+                                      child: Text(
+                                        "R\$ 12.0000,00",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 10),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Text(
+                        "Pegar\nEmpréstimo",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 232, 232, 232),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            color: Colors.black,
+                            Icons.money,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Text(
+                        "Transferir",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 232, 232, 232),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            color: Colors.black,
+                            Icons.pix,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Text(
+                        "Pix",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 232, 232, 232),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            color: Colors.black,
+                            Icons.pix,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 5),
+                      child: Text(
+                        "Pix",
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
